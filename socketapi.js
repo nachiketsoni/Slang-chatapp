bb39c0dff94331f8c5438f5af28730c17c5decec
nachiketsoni <nxchikxt@gmail.com>
2022-06-01 22:42:42 +0530

    jai ho

--- socketapi.js
+++ socketapi.js
@@ -1,20 +1,32 @@
-const io = require( "socket.io" )();
+const { use } = require("express/lib/application");
+
+const io = require("socket.io")();
 const socketapi = {
     io: io
 };
 
-const user = {}
+const user = []
 
 // Add your socket.io logic here!
-io.on( "connection", function( socket ) {
-    console.log( "A user connected"  );
-    socket.on('newUserJoined', (name)=>{
-              user[socket.id] = name
-              socket.broadcast.emit('user-joined', name)
-    } )
-    socket.on('send', (message)=>{
-              socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
-    } )
+io.on("connection", function (socket) { 
+    socket.on('newUserJoined', (name) => {
+        
+        user.push(name)
+        socket.broadcast.emit('user-joined', name)
+        io.emit('alluser', user)
+        console.log(user)
+        socket.on('disconnect', function () {
+            socket.broadcast.emit('user-disconnected',name)
+            let index = user.indexOf(name)
+            user.splice(index , 1)
+            io.emit('alluser', user)
+        })
+        
+        socket.on('send', (message) => {
+            socket.broadcast.emit('receive', { message: message, name: name })
+        })
+
+    })
 
 });
 // end of socket.io logic
